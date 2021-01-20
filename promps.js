a67e9d2f48b4edc5b55fd30697a5b37b52bde92d
Alexander Thompson <alexthompson429@gmail.com>
2021-01-19 20:57:11 -0700

    task addition

---
+++ promps.js
@@ -0,0 +1,25 @@
+firstPrompt: {
+    type: "list",
+    name: "task",
+    message: "Make a selection:",
+    choices: [
+        /* === || VIEW || === */
+        "View Employees", // viewEmployee();
+        "View Employees by Manager", // viewEmployeeByManager();
+        "View Employees by Department", // viewEmployeeByDepartment();
+        "View Departments", // viewDepartments();
+        "View Roles", // viewRoles();
+        "View Department Budget", // viewDepartmentBudget();
+        /* === || ADD || === */
+        "Add Employee", // addEmployee();
+        "Add Department", // addDepartment();
+        "Add Role", // addRole();
+        /* === || UPDATE || === */
+        "Update Employee Role", // updateEmployeeRole();
+        "Update Employee Manager", // updateEmployeeManager();
+        /* === || REMOVE || === */
+        "Remove Employee", // removeEmployees();
+        "Remove Department", // removeDepartment();
+        "Remove Role", // removeRole();
+        /* === || EXIT || === */
+        "Exit",
